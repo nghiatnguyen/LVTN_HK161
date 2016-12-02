654ab020086c12f1f9927d5413ca4007ba467d27
trongnghianguyen1994 <trongnghianguyen1994@gmail.com>
2016-12-02 17:07:50 +0700

    Update

--- TOOLS/COREFERENCE_RESOLVER/src/coreferenceresolver/test/CrossValidation.java
+++ TOOLS/COREFERENCE_RESOLVER/src/coreferenceresolver/test/CrossValidation.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import coreferenceresolver.element.CorefChain;
 import coreferenceresolver.element.NounPhrase;
 import coreferenceresolver.element.Review;
 import coreferenceresolver.test.Evaluation;
@@ -22,8 +23,17 @@ import coreferenceresolver.util.Util;
 import coreferenceresolver.weka.Weka;
 
 public class CrossValidation {
+	
+	private static int kFold = 5;
+	
+	public static int getkFold(){
+		return kFold;
+	}
+	public static void main(String[] args) throws Exception{
+		File inputFile = new File("E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\input.txt");
         File markupFile = new File("E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\input.txt.markup");
         StanfordUtil su = new StanfordUtil(inputFile);
+        
 
         Util.readDataset();
         try {
@@ -52,70 +62,68 @@ public class CrossValidation {
 
             //Initialize sentiment and comparatives for each NP in each review
             Util.initSentimentAndComparativesForNPs();
-
+            
             //Set list instances of each review
-            for (Review re : StanfordUtil.reviews) {
-                Util.setInstancesForReviews(re);
-            }
-
+            for (Review re : StanfordUtil.reviews)
+            	Util.setInstancesForReviews(re);
+            
             //Create list random element's order
             ArrayList<Integer> shuffleList = new ArrayList<>();
-            for (int i = 0; i < StanfordUtil.reviews.size(); i++) {
-                shuffleList.add(i);
+    		for (int i = 0; i < StanfordUtil.reviews.size(); i++)
+    			shuffleList.add(i);
+    		Collections.shuffle(shuffleList);
+            
+    		ArrayList<ArrayList<Review>> listForTrains = new ArrayList<ArrayList<Review>>();
+    		ArrayList<ArrayList<Review>> listForTests = new ArrayList<ArrayList<Review>>();
+    		//Create the kFold dataset
+            for (int i = 1; i <= kFold; i++){
+	            ArrayList<Review> listForTrain = new ArrayList<>();
+	            ArrayList<Review> listForTest = new ArrayList<>();
+	            int sizeEachFold = StanfordUtil.reviews.size()/kFold + 1;
+	            for (int j = 0; j < StanfordUtil.reviews.size(); j++){
+	            	if (j >= i*sizeEachFold - sizeEachFold && j < i*sizeEachFold)
+	            		listForTest.add(StanfordUtil.reviews.get(shuffleList.get(j)));
+	            	else
+	            		listForTrain.add(StanfordUtil.reviews.get(shuffleList.get(j)));
+	            }
+	            listForTrains.add(listForTrain);
+	            listForTests.add(listForTest); 
+	            String strTrainFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\trainFile" + i + ".arff";
+	            String strTestFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\testFile" + i + ".arff";
+	            File outputTrainFile = new File(strTrainFile);
+	            try {
+	            	System.out.println("Writing the fileTrain " + i);
+					run(listForTrain, strTrainFile, true);
+					System.out.println("Done fileTrain " + i);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            File outputTestFile = new File(strTestFile);
+	            try {
+	            	System.out.println("Writing the fileTest " + i);
+					run(listForTest, strTestFile, false);
+					System.out.println("Done fileTest " + i);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
             }
-            Collections.shuffle(shuffleList);
-
-            ArrayList<ArrayList<Review>> listForTrains = new ArrayList<ArrayList<Review>>();
-            ArrayList<ArrayList<Review>> listForTests = new ArrayList<ArrayList<Review>>();
-            //Create the kFold dataset
-            for (int i = 1; i <= kFold; i++) {
-                ArrayList<Review> listForTrain = new ArrayList<>();
-                ArrayList<Review> listForTest = new ArrayList<>();
-                int sizeEachFold = StanfordUtil.reviews.size() / kFold + 1;
-                for (int j = 0; j < StanfordUtil.reviews.size(); j++) {
-                    if (j >= i * sizeEachFold - sizeEachFold && j < i * sizeEachFold) {
-                        listForTest.add(StanfordUtil.reviews.get(shuffleList.get(j)));
-                    } else {
-                        listForTrain.add(StanfordUtil.reviews.get(shuffleList.get(j)));
-                    }
-                }
-                listForTrains.add(listForTrain);
-                listForTests.add(listForTest);
-                String strTrainFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\trainFile" + i + ".arff";
-                String strTestFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\testFile" + i + ".arff";
-                File outputTrainFile = new File(strTrainFile);
-                try {
-                    System.out.println("Writing the fileTrain " + i);
-                    run(listForTrain, strTrainFile, true);
-                    System.out.println("Done fileTrain " + i);
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                File outputTestFile = new File(strTestFile);
-                try {
-                    System.out.println("Writing the fileTest " + i);
-                    run(listForTest, strTestFile, false);
-                    System.out.println("Done fileTest " + i);
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-
+            
+            
             //Run the kFold
-            for (int i = 1; i <= kFold; i++) {
-                String strTrainFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\trainFile" + i + ".arff";
-                String strTestFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\testFile" + i + ".arff";
-                String strResultFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\result.txt";
-                Weka.j48Classify(strTrainFile, strTestFile, strResultFile);
-                System.out.println("-----Fold " + i + "-----");
-                Evaluation.scoreMUC(listForTests.get(i - 1));
-                Evaluation.scoreB3(listForTests.get(i - 1));
+            for (int i = 1; i <= kFold; i++){
+            	 String strTrainFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\trainFile" + i + ".arff";
+ 	            String strTestFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\testFile" + i + ".arff";
+ 	            String strResultFile = "E:\\REPOSITORIES\\LVTN_HK161\\DATASET\\result.txt";
+            	Weka.j48Classify(strTrainFile, strTestFile, strResultFile);
+            	System.out.println("-----Fold " + i + "-----");
+            	Evaluation.scoreMUC(listForTests.get(i-1));
+            	Evaluation.scoreB3(listForTests.get(i-1));
             }
-
+            
             Evaluation.resultFinal();
-
+            
         } catch (IOException ex) {
             Logger.getLogger(MarkupMain.class.getName()).log(Level.SEVERE, null, ex);
         }
